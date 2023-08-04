87a4fb4302424705fcfbd3f3d147793ad68c5db6
sudheer2144 <sudheer.ged@gmail.com>
2023-08-04 14:31:05 +0530

    Update

--- public/script.js
+++ public/script.js
@@ -1,51 +1,62 @@
+const socket = io();
 
-const socket=io();
+let senderName = "";
 
-let username='';
+let receiverName = "";
 
 let button = document.getElementById("join-btn");
 
-button.addEventListener('click',(event)=>{
-    // console.log("test")
-    event.preventDefault();//preventing the default behaviours
-    username=document.getElementById("username").value;
-    if(username.trim()!==""){
-        document.querySelector(".form-username").style.display="none";
-        document.querySelector(".chatroom-container").style.display="block";
-    }
+button.addEventListener("click", (event) => {
+  event.preventDefault(); //preventing the default behaviours
+
+  console.log("clicked");
+
+  let input = document.getElementById("username").value;
+
+  if (input.trim() !== "" && input.split(" ").length === 2) {
+    senderName = input.split(" ")[0];
+    receiverName = input.split(" ")[1];
+    document.querySelector(".form-username").style.display = "none";
+    document.querySelector(".chatroom-container").style.display = "block";
+  } else {
+    alert("Enter valid input");
+  }
 });
 
-document.getElementById("send-button").addEventListener('click',(event)=>{
-    event.preventDefault();
+document.getElementById("send-button").addEventListener("click", (event) => {
+  event.preventDefault();
 
-    const data = {
-        userName : username,
-        message : document.getElementById("message-input").value
-    }
+  const data = {
+    sender_userName: username,
+    message: document.getElementById("message-input").value,
+    receiver_userName: receiverName,
+  };
 
-    //if 'io' is emitting anything 'sockets' can listen
-    //if 'socket' is emitting anything only 'io' can listen
+  //if 'io' is emitting anything 'sockets' can listen
+  //if 'socket' is emitting anything only 'io' can listen
 
-    //sending message to io
-    socket.emit('message',data);
+  //sending message to io
+  socket.emit("message", data);
 
-    //adding the sent message to ui
-    addMessageToUI(data,"sent");
+  //adding the sent message to ui
+  addMessageToUI(data, "sent");
 });
 
-//receiving message from io
-socket.on('message',(data)=>{
-    if(data.userName!==username){//if the sender username same as the current username it should not print the message
-        addMessageToUI(data,"received");
-    }
+// receiving message from io
+socket.on("message", (data) => {
+  console.log(data);
+  if (data.receiver_userName === senderName) {
+    //if the sender username same as the current username it should not print the message
+    addMessageToUI(data, "received");
+  }
 });
 
-function addMessageToUI(data,type){
-    let receivedMessage = document.createElement("div");
-    receivedMessage.setAttribute("class",`message ${type}`);
-    receivedMessage.innerText=data.message;
-    document.getElementById("message-container").appendChild(receivedMessage);
-    if(type==="sent"){
-        document.getElementById("message-input").value="";
-    }
+function addMessageToUI(data, type) {
+  let receivedMessage = document.createElement("div");
+  receivedMessage.setAttribute("class", `message ${type}`);
+  receivedMessage.innerText = data.message;
+  document.getElementById("message-container").appendChild(receivedMessage);
+  if (type === "sent") {
+    document.getElementById("message-input").value = "";
+  }
 }
--- server.js
+++ server.js
@@ -1,37 +1,39 @@
 //import express
-const express=require('express');
+const express = require("express");
 
 //make an express app
-const app=express();
-
-//giving protocol for server and integrate the app got from express
-const server=require('http').Server(app);
-
-//set the public folder to express
-app.use(express.static('public'));
-
-//integrate socket to server
-const io=require("socket.io")(server);
-
-//server will be having io
-io.on('connection',(socket)=>{
-    console.log("connection established:",socket.id);
-
-    //socketA -> io -> socketB
-    socket.on('message',(data)=>{ //user is sendig message
-        io.emit('message',data);//emitting this message to all other sockets
-    });
-
-    //if any socket is disconnected this will print its id
-    socket.on('disconnect',()=>{//showing off that user left the chat
-        console.log(socket.id," Left chat");
-    });
-});
+const app = express();
 
 //setting port for the server
-const port=8500;
+const port = 8500;
+
+//giving protocol for server and integrate the app got from express
+const server = require("http").Server(app);
 
 //making the server listen to the port 8500
-server.listen(port,()=>{
-    console.log(`server is running on port ${port}`);
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+//set the public folder to express
+app.use(express.static("public"));
+
+//integrate socket to server
+const io = require("socket.io")(server);
+
+//server will be having io
+io.on("connection", (socket) => {
+  console.log("connection established:", socket.id);
+
+  //socketA -> io -> socketB
+  socket.on("message", (data) => {
+    //user is sendig message
+    io.emit("message", data); //emitting this message to all other sockets
+  });
+
+  //if any socket is disconnected this will print its id
+  socket.on("disconnect", () => {
+    //showing off that user left the chat
+    console.log(socket.id, " Left chat");
+  });
 });
